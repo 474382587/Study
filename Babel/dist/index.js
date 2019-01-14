75821c386046644e278b64db44d6359803537264
Joseph Jin <jerkjoe61@gmail.com>
2019-01-13 23:47:46 -0800

    .

--- Babel/dist/index.js
+++ Babel/dist/index.js
@@ -4,4 +4,6 @@ var sayHi = function sayHi() {
   console.log('Babel');
 };
 
+var num = 3;
+var str = "I am ".concat(num, " years old");
 sayHi();
--- Babel/src/index.js
+++ Babel/src/index.js
@@ -2,4 +2,9 @@ const sayHi = () => {
     console.log('Babel')
 }
 
+const num = 3
+
+const str = `I am ${num} years old`
+
+
 sayHi()
