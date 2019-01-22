8bede2161d5270045c52eb11eb85d03ab1e8180f
Joseph Jin <jerkjoe61@gmail.com>
2019-01-21 23:47:45 -0800

    express

---
+++ express/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3030
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+app.get('/new', (req, res) => {
+    res.send('<h1>New Hello World!</h1>')
+})
+
+app.listen(port, () => {
+    console.log('Server Started!    ' + port)
+})
