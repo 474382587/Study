5d7534791f4169855dda169e17d6709ea9f48251
Joseph Jin <jerkjoe61@gmail.com>
2019-01-24 23:43:20 -0800

    node server

---
+++ simpleserver/server.js
@@ -0,0 +1,55 @@
+const http = require('http')
+const url = require('url')
+const path = require('path')
+const fs = require('fs')
+
+const mimeTypes = {
+    html: 'text/html',
+    jpeg: 'image/jpeg',
+    png: 'image/png',
+    js: 'text/javascript',
+    css: 'text/css'
+}
+
+var app = http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname
+    var fileName = path.join(process.cwd(), unescape(uri))
+    console.log('loading ' + uri)
+    var stats
+    try {
+        stats = fs.lstatSync(fileName)
+    } catch (e) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' })
+        res.write('404 Not Found\n')
+        res.end()
+        return
+    }
+
+    if (stats.isFile()) {
+        console.log(path
+            .extname(fileName)
+            .split()
+            .reverse()[0])
+        var mimeType =
+            mimeTypes[
+                path
+                    .extname(fileName)
+                    .split('.')
+                    .reverse()[0]
+            ]
+        res.writeHead(200, { 'Content-Type': mimeType })
+        var fileStream = fs.createReadStream(fileName)
+        fileStream.pipe(res)
+    } else if (stats.isDirectory()) {
+        res.writeHead(302, { Location: 'index.html' })
+        res.end()
+    } else {
+        res.writeHead(500, { 'Content-Type': 'text/plain' })
+        res.write('500 Internal Server Error\n')
+        res.end()
+    }
+})
+
+app.listen(1337, () => {
+    console.log('Server Started!')
+})
