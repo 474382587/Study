e4a9b23c15d5c9462ab176afd9964d3e4ffcc150
Joseph J <jerkjoe61@gmail.com>
2019-01-24 16:49:09 -0800

    .

--- express/server.js
+++ express/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 
 const app = express()
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 const port = process.env.PORT || 3030
 
 app.get('/', (req, res) => {
@@ -10,6 +14,12 @@ app.get('/', (req, res) => {
 app.get('/new', (req, res) => {
     res.send('<h1>New Hello World!</h1>')
 })
+
+app.post('/', (req, res) => {
+    var a = req
+    res.send('')
+})
+
 
 app.listen(port, () => {
     console.log('Server Started! ' + port)
