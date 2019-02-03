9c4837a7890b9e31ef21725bce9ef281f4022390
Joseph Jin <jerkjoe61@gmail.com>
2019-02-02 19:54:06 -0800

    .

--- async/asyncbasics/basics.js
+++ async/asyncbasics/basics.js
@@ -2,11 +2,22 @@
 // import request from 'request'
 const request = require('request')
 const yargs = require('yargs')
-
-
-request({
-    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=agbYq8Qc12YtWPHLCvYv5uJAWDTaHUPQ&location=1301%20lombard%20street%20philadelphia   ',
-    json:true
-}, (err, res, body) => {
-    console.log(JSON.stringify(body.results, null, 4))
-})
+const argv = yargs.options({
+    a: {
+        demand:true,
+        alias: 'address',
+        desc: 'Address to fetch weather for',
+        string: true
+    }
+}).help().alias('help', 'h').argv
+console.log(process.argv)
+request(
+    {
+        url:
+            'http://www.mapquestapi.com/geocoding/v1/address?key=agbYq8Qc12YtWPHLCvYv5uJAWDTaHUPQ&location=1301%20lombard%20street%20philadelphia   ',
+        json: true
+    },
+    (err, res, body) => {
+        console.log(JSON.stringify(body.results, null, 4))
+    }
+)
