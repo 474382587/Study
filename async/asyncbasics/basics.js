fdaa74c7962fdabc490591f875907f49b0d259b7
Joseph Jin <jerkjoe61@gmail.com>
2019-02-03 23:52:13 -0800

    .

--- async/asyncbasics/basics.js
+++ async/asyncbasics/basics.js
@@ -2,19 +2,25 @@
 // import request from 'request'
 const request = require('request')
 const yargs = require('yargs')
-const argv = yargs.options({
-    a: {
-        demand:true,
-        alias: 'address',
-        desc: 'Address to fetch weather for',
-        string: true
-    }
-}).help().alias('help', 'h').argv
-console.log(process.argv)
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            desc: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h').argv
+
+const encodedAddress = encodeURI(argv.address)
+console.log(encodedAddress)
 request(
     {
         url:
-            'http://www.mapquestapi.com/geocoding/v1/address?key=agbYq8Qc12YtWPHLCvYv5uJAWDTaHUPQ&location=1301%20lombard%20street%20philadelphia   ',
+            'http://www.mapquestapi.com/geocoding/v1/address?key=agbYq8Qc12YtWPHLCvYv5uJAWDTaHUPQ&location=' +
+            encodedAddress,
         json: true
     },
     (err, res, body) => {
