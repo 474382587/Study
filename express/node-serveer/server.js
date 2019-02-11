b7b437a2340f2724c55d288ce499d313cdfe37c7
Joseph Jin <jerkjoe61@gmail.com>
2019-02-10 16:33:52 -0800

    node server

---
+++ express/node-serveer/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const hbs = require('hbs')
+const port = process.env.PORT || 3000
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+hbs.registerHelper('screamIt', text => {
+    return text.toUpperCase()
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs')
+
+// })
+app.set('view_engine', hbs)
+console.log('dirname: ', __dirname)
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'This is Home Page!'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'Hello HBS Users!'
+    })
+})
+
+app.listen(port, () => {
+    console.log('start at port ' + port)
+})
