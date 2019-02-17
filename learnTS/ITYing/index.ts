a6e3ecf496c786f4598f89f1e87c6705309b5b5d
Joseph Jin <jerkjoe61@gmail.com>
2019-02-16 23:12:30 -0800

    ng

--- learnTS/ITYing/index.ts
+++ learnTS/ITYing/index.ts
@@ -1,3 +1,24 @@
-var str: string = '123';
-var a = 3
-a = 's'
+interface Config {
+    (val: number):string
+}
+
+function numberToString() {
+    
+}
+
+var a:Config = function(hello){
+    return hello + ''
+}
+
+interface c {
+    say(hi:string):string
+}
+
+class d implements c {
+    constructor() {
+        
+    }
+    say(hi:string) {
+        return hi
+    }
+}
