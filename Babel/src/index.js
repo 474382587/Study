98acb0819909c03c8d2e543930f549f5b95e0b25
Joseph Jin <jerkjoe61@gmail.com>
2019-03-01 22:46:27 -0800

    。

--- Babel/src/index.js
+++ Babel/src/index.js
@@ -1,11 +1,11 @@
 const sayHi = () => {
-    console.log('Babel')
-}
+    console.log('Babel');
+};
 
-const num = 3
+const num = 3;
 
-const str = `I am ${num} years old`
+const str = `I am ${num} years old`;
 
-const arr1 = [1,3,2]
-const arr2 = [1, ...arr1]
-sayHi()
+const arr1 = [1, 3, 2];
+const arr2 = [1, ...arr1];
+sayHi();
