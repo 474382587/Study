95a96ef95d61a57514b5e5eff4b684f8ae12d061
Joseph Jin <jerkjoe61@gmail.com>
2019-03-06 22:48:00 -0800

    .

--- async/asyncbasics/basics.js
+++ async/asyncbasics/basics.js
@@ -1,9 +1,9 @@
 // HTTP Request library
 // import request from 'request'
 // const request = require('request')
-const geocode = require('./geocode/geocode')
-const weather = require('./weather/weather')
-const yargs = require('yargs')
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+const yargs = require('yargs');
 const argv = yargs
     .options({
         a: {
@@ -14,14 +14,14 @@ const argv = yargs
         }
     })
     .help()
-    .alias('help', 'h').argv
+    .alias('help', 'h').argv;
 
 // const encodedAddress = encodeURI(argv.address)
 // console.log(encodedAddress)
 
 geocode.geocodeAddress(argv.address, function(errMsg, results) {
     if (errMsg) {
-        console.log(errMsg)
+        console.log(errMsg);
     } else {
         // console.log(JSON.stringify(results[0].locations[0].latLng, undefined, 2))
         weather.getWeather(
@@ -29,14 +29,18 @@ geocode.geocodeAddress(argv.address, function(errMsg, results) {
             results[0].locations[0].latLng.lng,
             function(err, result) {
                 if (err) {
-                    console.log(err)
+                    console.log(err);
                 } else {
-                    console.log(JSON.stringify(result.temperature, undefined, 2))
-                    console.log(JSON.stringify(result.apparentTemperature, undefined, 2))
+                    console.log(
+                        JSON.stringify(result.temperature, undefined, 2)
+                    );
+                    console.log(
+                        JSON.stringify(result.apparentTemperature, undefined, 2)
+                    );
                 }
             }
-        )
+        );
     }
-})
+});
 
 // weather.getWeather(37.8267, -122.4233)
