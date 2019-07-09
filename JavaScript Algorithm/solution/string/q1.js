dc1981ff902f811ad435a010ba1754efe68515e6
Joseph Jin <jerkjoe61@gmail.com>
2019-07-08 22:28:36 -0700

    reverse string

---
+++ JavaScript Algorithm/solution/string/q1.js
@@ -0,0 +1,15 @@
+// export default function reverseWords(word) {
+//   return word.split(/\s/g).map(e =>
+//     e.split('')
+//       .reverse()
+//       .join('')
+//   ).join(' ')
+// }
+
+export default function reverseWords(word) {
+  return word.match(/\S+/g).map(e =>
+    e.split('')
+      .reverse()
+      .join('')
+  ).join(' ')
+}
