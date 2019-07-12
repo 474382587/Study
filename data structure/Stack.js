9932e3fd281c2c794d6d13bb21decde47b3bc176
Joseph Jin <jerkjoe61@gmail.com>
2019-07-12 14:55:02 -0700

    add other methods

--- data structure/Stack.js
+++ data structure/Stack.js
@@ -16,6 +16,18 @@ class Stack {
     pop() {
         return this.array.splice(this.array.length - 1, 1)
     }
+    
+    peek() {
+        return this.array[this.array.length - 1]
+    }
+    
+    empty() {
+        return this.array.length > 0
+    }
+    
+    size() {
+        return this.array.length
+    }
 
     print() {
         console.log(this.array)
