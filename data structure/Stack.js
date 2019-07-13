e929581d10b5f521a3d03d702d24c7f072267cd1
Joseph Jin <jerkjoe61@gmail.com>
2019-07-13 15:04:30 -0700

    error message

--- data structure/Stack.js
+++ data structure/Stack.js
@@ -6,7 +6,11 @@
 
 class Stack {
     constructor(array) {
-        this.array = array
+        if (Object.prototype.toString.call(array) === '[object Array]') {
+            this.array = array
+        } else {
+            throw new Error('Not Array')
+        }
     }
 
     push(item) {
@@ -16,15 +20,15 @@ class Stack {
     pop() {
         return this.array.splice(this.array.length - 1, 1)
     }
-    
+
     peek() {
         return this.array[this.array.length - 1]
     }
-    
+
     empty() {
         return this.array.length > 0
     }
-    
+
     size() {
         return this.array.length
     }
