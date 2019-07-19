92a7107d046c7ca7b5f4c8111740247802b57b0f
Joseph Jin <jerkjoe61@gmail.com>
2019-07-18 23:30:48 -0700

    readme + remove console + add comment

--- account-display/src/components/Account/Account.js
+++ account-display/src/components/Account/Account.js
@@ -1,97 +1,107 @@
-import React, { Component } from 'react'
-import axios from 'axios' // Request library
-import { Spin, Divider, Card } from 'antd'
-import { Link } from "react-router-dom";
-
-
+import React, { Component } from 'react';
+// React-Router
+import { Link } from 'react-router-dom';
+// Request library
+import axios from 'axios';
+// Utils
+import { ERROR, openNotification } from '../../utils/notification';
 // UI Components
-import { Select } from 'antd';
+import { Spin, Divider, Card, Select } from 'antd';
 const { Option } = Select;
 
 class Account extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accountsArray: [],
+      selectedAccount: '',
+      loading: true
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            accountsArray: [],
-            selectedAccount: '',
-            loading: true
-        }
-    }
+  componentDidMount() {
+    // Retrieve data from server
+    axios
+      .get('/accounts')
+      .then(res => {
+        const accounts = res.data;
+        this.setState({
+          loading: false,
+          accountsArray: accounts
+        });
+      })
+      .catch(err => {
+        //   error handling
+        console.log('err', err);
+        openNotification(ERROR);
+      });
+  }
 
-    onChange = (value) => {
-        console.log(`selected ${value}`);
-    }
+  // Select Account callback
+  handleSelectAccount = account => {
+    this.setState(state => {
+      return {
+        selectedAccount: state.accountsArray.find(
+          item => item.id === account.key - 0
+        )
+      };
+    });
+  };
 
-    onBlur = () => {
-        console.log('blur');
-    }
+  render() {
+    return this.state.loading ? (
+      <div className="align-center">
+        <Spin size="large" />
+      </div>
+    ) : (
+      <div className="container">
+        <h2>Please select an account:</h2>
+        <Select
+          showSearch
+          className="full-width"
+          placeholder="Select a person"
+          optionFilterProp="children"
+          filterOption={(input, option) =>
+            option.props.children.toLowerCase().indexOf(input.toLowerCase()) >=
+            0
+          }
+        >
+          {this.state.accountsArray.map(account => {
+            return (
+              <Option
+                onClick={this.handleSelectAccount}
+                key={account.id}
+                value={account.id}
+              >
+                {account.name}
+              </Option>
+            );
+          })}
+        </Select>
 
-    onFocus = () => {
-        console.log('focus');
-    }
-
-    onSearch = (val) => {
-        console.log('search:', val);
-    }
-
-    componentDidMount() {
-        axios.get('/accounts').then((res) => {
-            const accounts = res.data
-            this.setState({
-                loading: false,
-                accountsArray: accounts
-            })
-        }).catch(err => {
-            console.log('err', err)
-            this.openNotification()
-        })
-    }
-    handleSelectAccount = (account) => {
-        console.log(account)
-        console.log(account.key)
-        this.setState(state => {
-            console.log(state.accountsArray)
-            return {
-                selectedAccount: state.accountsArray.find(item => item.id === (account.key - 0))
-            }
-        })
-    }
-
-    render() {
-        return this.state.loading ? <div style={{ textAlign: 'center' }}><Spin size="large" /></div> : (<div className="container" >
-            <Select
-                showSearch
-                className="full-width"
-                placeholder="Select a person"
-                optionFilterProp="children"
-                onChange={this.onChange}
-                onFocus={this.onFocus}
-                onBlur={this.onBlur}
-                onSearch={this.onSearch}
-                filterOption={(input, option) =>
-                    option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                }
+        {this.state.selectedAccount ? (
+          <div>
+            <Divider />
+            <Card
+              title="Account"
+              extra={
+                <Link to={`account/${this.state.selectedAccount.id}`}>
+                  More Detail
+                </Link>
+              }
+              className="full-width"
             >
-                {
-                    this.state.accountsArray.map((account, index) => {
-                        return (<Option onClick={this.handleSelectAccount} key={account.id} value={account.id}>{account.name}</Option>)
-                    })
-                }
-            </Select>
-
-            {
-                this.state.selectedAccount ? (
-
-                    <div>
-                        <Divider />
-                        <Card title="Account" extra={<Link to={`account/${this.state.selectedAccount.id}`}>More Detail</Link>} className="full-width" >
-                            <span className="special-content">{this.state.selectedAccount.name}</span>
-                        </Card>
-                    </div>) : ''
-            }
-        </div>)
-    }
+              <span className="special-content">
+                {this.state.selectedAccount.name}
+              </span>
+            </Card>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    );
+  }
 }
 
-export default Account 
+export default Account;
--- account-display/src/router.js
+++ account-display/src/router.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React from 'react';
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Account from './components/Account/Account'
-import AccountDetail from './components/AccountDetail/AccountDetail'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+import Account from './components/Account/Account';
+import AccountDetail from './components/AccountDetail/AccountDetail';
 
 function AppRouter() {
-    return (<Router>
+  return (
+    <Router>
+      <Switch>
         <Route path="/" exact component={Account} />
-        <Route path="/account/:id" component={AccountDetail}></Route>
-    </Router>)
+        <Route path="/account/:id" component={AccountDetail} />
+        <Redirect from="/index.html" to="/" />
+        <Redirect from="/index.html" to="/" exact />
+      </Switch>
+    </Router>
+  );
 }
 
-export default AppRouter
+export default AppRouter;
