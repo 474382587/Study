d92c476285789698d8985a60fbc23702910b2f75
Joseph Jin <jerkjoe61@gmail.com>
2019-07-20 15:09:19 -0700

    Queue

---
+++ data structure/Queue.js
@@ -0,0 +1,28 @@
+
+
+
+class Queue {
+    constructor(array) {
+        if (Object.prototype.toString.call(array) === '[object Array]') {
+            this.array = array
+        } else {
+            throw new Error('Not Array! Please modify your input.')
+        }
+    }
+
+    enqueue(item) {
+        this.array.push(item)
+    }
+    dequeue() {
+        return this.array.splice(0, 1)
+    }
+    peek() {
+        return this.array[0]
+    }
+    isEmpty() {
+        return this.array.length > 0
+    }
+    getLength() {
+        return this.array.length
+    }
+} 
