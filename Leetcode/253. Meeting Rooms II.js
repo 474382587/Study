ba2235c068796b7b4afae04041583f73008f481b
Joseph Jin <jerkjoe61@gmail.com>
2019-10-17 23:26:31 -0700

    2

--- Leetcode/253. Meeting Rooms II.js
+++ Leetcode/253. Meeting Rooms II.js
@@ -1,2 +1,38 @@
     
+    /**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var minMeetingRooms = function(intervals) {
+
+    let container = {}
+    intervals.sort((a, b) => a[0] - b[0])
+  
+    // let rooms = []
     
+    for(let i = 0; i < intervals.length; i++) {
+      console.log('I am here!111')
+      if(intervals[i].length === 0) {
+        console.log('I am here2!')
+        continue
+      } else {
+        for(let j = i + 1; j < intervals.length; j++) {
+          console.log('I am here!', j)
+          if(intervals[i][1] <= intervals[j][0]) {
+            intervals[i][1] = intervals[j][1]
+            intervals[j] = []
+          }
+        }
+      }
+    }
+    console.log(intervals)
+    let count = 0
+    intervals.forEach(e => {
+      if(e.length !== 0) {
+        count++
+      }
+    })
+    
+  
+    return count
+  };
