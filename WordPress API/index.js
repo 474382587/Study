e311f1310c5db5ad88e945ab78b2498db0c16ff8
Joseph Jin <jerkjoe61@gmail.com>
2019-11-06 15:20:38 -0800

    .

--- WordPress API/index.js
+++ WordPress API/index.js
@@ -1,5 +1,5 @@
-console.log(axios)
+console.log(axios);
 // http://webdevjoe.com/wp-json/wp/v2/posts
 axios.get('http://webdevjoe.com/wp-json/wode/v1/action/').then(res => {
-    console.log(res)
-})
+    console.log(res);
+});
