6dea75f40cb7396f82bc60c9cadd9d6d58765ff9
Joseph Jin <jerkjoe61@gmail.com>
2019-12-19 00:09:30 -0800

    242. Valid Anagram

---
+++ Leetcode/242. Valid Anagram.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    
+    var s = s.split('').sort().join()
+    var t = t.split('').sort().join()
+    
+    return s === t
+    
+};
