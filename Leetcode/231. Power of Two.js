ea10c156597156b4d092af69f281a40972260abf
Joseph Jin <joseph.jin@belmar.ca>
2019-12-20 14:57:17 -0800

    231. Power of Two

---
+++ Leetcode/231. Power of Two.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n < 2) {
+        return n === 1
+    } else {
+        return isPowerOfTwo(n / 2)
+    }
+};
