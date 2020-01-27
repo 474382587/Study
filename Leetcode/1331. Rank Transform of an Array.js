1f236bb18497bed87f771f24ec3980e155309364
Joseph Jin <jerkjoe61@gmail.com>
2020-01-27 09:06:16 -0800

    1331. Rank Transform of an Array

---
+++ Leetcode/1331. Rank Transform of an Array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function(arr) {
+    var hash = {}
+    var arr2 = []
+    for(var i = 0; i < arr.length; i++) {
+        // console.log(hash[arr[i]])
+        if(hash[arr[i]] === undefined) {
+            arr2.push(arr[i])
+            hash[arr[i]] = true
+        }
+    }
+    // console.log(arr2)
+    arr2.sort((a, b) => a - b)
+    for(var i = 0; i < arr2.length; i++) {
+        hash[arr2[i]] = i + 1
+    }
+    
+    for(var i = 0; i < arr.length; i++) {
+        arr[i] = hash[arr[i]]
+    }
+    
+    return arr
+};
