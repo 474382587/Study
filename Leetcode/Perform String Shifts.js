7caca494e92b216624e282e16b0e40e25c77cab7
Joseph Jin <jerkjoe61@gmail.com>
2020-04-21 09:26:33 -0700

    12

--- Leetcode/Perform String Shifts.js
+++ Leetcode/Perform String Shifts.js
@@ -3,22 +3,22 @@
  * @param {number[][]} shift
  * @return {string}
  */
-var stringShift = function(s, shift) {
+var stringShift = function (s, shift) {
     var hash = {
         left: 0,
         right: 0
     }
-    for(let op of shift) {
-        if(op[0] === 1) {
+    for (let op of shift) {
+        if (op[0] === 1) {
             hash.right += op[1]
         } else {
             hash.left += op[1]
         }
-    }  
+    }
     console.log((hash.right - hash.left) % s.length)
     var finalOp = ((hash.right - hash.left) % s.length) >= 0 ? ((hash.right - hash.left) % s.length) : s.length + ((hash.right - hash.left) % s.length)
-    
+
     console.log(finalOp)
-    return s.substr(s.length-finalOp, s.length) + s.substr(0, s.length-finalOp)
-    
+    return s.substr(s.length - finalOp, s.length) + s.substr(0, s.length - finalOp)
+
 };
